f3607a30c9ed7269c9a57dfa5bb495e95514ad1e
Natália <natalia.guimaraesbello@gmail.com>
2025-01-24 22:53:10 -0300

    Base criada
    Classe main foi criada, estruturando o que cada parte do menu fará

--- index.js
+++ index.js
@@ -1,15 +1,57 @@
-console.log('')
-console.log('------- Hotel F-Luxo -------')
-console.log('')
-console.log('Seja bem-vindo(a) ao site oficial do Hotel F-Luxo, \nonde a experiência da sua hospedagem é a nossa prioridade.')
-console.log('')
-console.log('1- Fazer uma reserva.')
-console.log('2- Fazer log-in.')
-console.log('3- Estadias Promocionais.')
-console.log('5- Informações adicionais.')
-console.log('')
-console.log('Digite a opcao desejada:')
-console.log('')
+var requisicao = require("readline-sync");
+
+
+
+//Classe principal onde vou chamar as outras classes e outros metodos
+class Main {
+    constructor() {
+        this.opcao = 0;
+    }
+
+    mostrarMenu(){
+        //MENU PRINCIPAL
+        console.log('')
+        console.log('------- Hotel F-Luxo -------')
+        console.log('')
+        console.log('Seja bem-vindo(a) ao site oficial do Hotel F-Luxo, \nonde a experiência da sua hospedagem é a nossa prioridade.')
+        console.log('')
+        console.log('1- Fazer uma reserva.')
+        console.log('2- Fazer log-in.')
+        console.log('3- Estadias Promocionais.')
+        console.log('4- Informações adicionais.')
+        console.log('5- Sair.')
+        console.log('')
+        var opcao = requisicao.question('Digite a opcao desejada: ')
+        console.log('')
+    }
+
+    main() {
+        while (this.opcao !== 4) {
+          this.mostrarMenu();
+          this.opcao = opcao
+    
+          switch (this.opcao) {
+            case 1:
+                // Chamar o método da reserva
+                break;
+            case 2:
+                // Chamar o método do log-in
+                break;
+            case 3:
+                // Chamar o metodo das estadias promocionais
+                break;
+            case 4:
+                // Chamar o método das informações adicionais 
+                break;
+            case 5:
+                console.log('Obrigada por acessar nosso site! Volte sempre.')
+                break;
+            default:
+              console.log("Opção inválida! Tente novamente.");
+          }
+        }
+    }
+}
 
 
 class Reserva{
@@ -19,6 +61,16 @@ class Reserva{
         this.status=status
         this.dataEntrada=dataEntrada
         this.dataSaida=dataSaida
+    }
+    verQuartos(){
+
+    }
+
+    cadastrar(){
+        var nomeClnt = requisicao.question('Digite seu nome: ')
+        var cpfClnt = requisicao.question('Digite seu CPF: ')
+        var emailClnt = requisicao.question('Digite seu e-mail: ')
+        var senhaClnt = requisicao.question('Digite uma senha: ')
     }
 }
 
@@ -46,6 +98,7 @@ class Cliente{
 }
 
 
+//Funcionarios poderao adiconar/remover quartos
 class Quartos{
     constructor(numCamas, preco, qntdDisp, nome, descricao){
 
@@ -54,6 +107,14 @@ class Quartos{
         this.qntdDisp=qntdDisp
         this.nome=nome
         this.descricao=descricao
+    }
+
+    adicionarQuarto(){
+        var numCamas = requisicao.question('Numero de camas disponiveis no quarto: ')
+        var preco = requisicao.question('Preco por noite: ')
+        var qntdDisp = requisicao.question('Quantidade de quartos disponiveis: ')
+        var nome = requisicao.question('Nome do quarto: ')
+        var descricao = requisicao.question('Descricao do quarto: ')
     }
 }
 
